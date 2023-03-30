b523fcb2f2bb45aada554d625befad8a14a6bb0b
Artur de Miranda Rodrigues <a224538@dac.unicamp.br>
2023-03-30 17:29:47 -0300

    validar cnpj feito

--- ArturSeguradorasLab03/src/ClientePJ.java
+++ ArturSeguradorasLab03/src/ClientePJ.java
@@ -10,7 +10,7 @@ public class ClientePJ extends Cliente{
 	{
 		super(nome, endereco, dataLicenca, educacao, genero, classeEconomica,
 													listaVeiculos);
-		if(validarCnpj(cnpj)) {
+		if(validarCNPJ(cnpj)) {
 			this.cnpj = cnpj;
 		}
 		this.dataFundacao = dataFundacao;
@@ -30,8 +30,53 @@ public class ClientePJ extends Cliente{
 		this.dataFundacao = dataFundacao;
 	}
 	
-	public boolean validarCnpj(String cnpj) {
-		return true; //fazer depois
-	}
+	public boolean validarCNPJ(String cnpj) {
+		String cnpjAux = cnpj.replaceAll("[^0-9]+", "");
+		
+		int PrimeiroDigitoVerificador = Character.getNumericValue(cnpjAux.charAt(12));
+		int SegundoDigitoVerificador = Character.getNumericValue(cnpjAux.charAt(13));
+		
+		if (cnpjAux.length() != 14)
+			return false;
+		
+		int somaAtual1 = 0;
+		
+		for (int i = 0, j = 5; i < 4 ; i++, j--) {
+			somaAtual1 += j * Character.getNumericValue(cnpjAux.charAt(i));
+		}
+		for (int i = 4, j = 9; i < 12; i++, j--) {
+			somaAtual1 += j * Character.getNumericValue(cnpjAux.charAt(i));
+		}
+		
+		if (somaAtual1 % 11 == 0 || somaAtual1 % 11 == 1) {
+			if (PrimeiroDigitoVerificador != 0) {
+				return false;
+			}
+			
+		}else if (11 - (somaAtual1 % 11) != PrimeiroDigitoVerificador) {
+			return false;
+		}
+		
+		int somaAtual2 = 0;
+		
+		for (int i = 0, j = 6; i < 5; i++, j--) {
+			somaAtual2 += j * Character.getNumericValue(cnpjAux.charAt(i));
+		}
 
+		for (int i = 5, j = 9; i < 13; i++, j--) {
+			somaAtual2 += j * Character.getNumericValue(cnpjAux.charAt(i));
+		}
+		
+		if (somaAtual2 % 11 == 0 || somaAtual2 % 11 == 1) {
+			if (SegundoDigitoVerificador != 0) {
+				return false;
+			}
+			
+		}else if (11 - (somaAtual2 % 11) != SegundoDigitoVerificador) {
+			return false;
+		}
+		
+		return true;
+	}
 }
+
